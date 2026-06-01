25ca9d3a2060db06987348df0c626e411c6e8559
NikeshSoni <nikesh21soni@gmail.com>
2026-06-01 20:22:06 +0530

    Changes

--- server.js
+++ server.js
@@ -11,11 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
-
-
-// || 'mongodb://localhost:27017/trade-journal'
-
 mongoose.connect(process.env.MONGODB_URI , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
